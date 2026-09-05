aec24c7d4c6fc0b3dcdb9c51096202b99e031383
Dmytro Valetskiy <dvaletskiy@gmail.com>
2026-09-05 11:29:31 +0300

    Fix logo styling and section navigation scrolling

--- src/hooks/useScrollToTop.js
+++ src/hooks/useScrollToTop.js
@@ -2,13 +2,15 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 export const useScrollToTop = () => {
-  const { pathname, hash } = useLocation();
+  const { pathname, hash, state } = useLocation();
 
   useEffect(() => {
+    if (pathname === "/" && ["items", "footer"].includes(state?.scrollTo)) return;
+
     window.scrollTo({
       top: 0,
       left: 0,
       behavior: "auto",
     });
-  }, [pathname, hash]);
+  }, [pathname, hash, state]);
 };
